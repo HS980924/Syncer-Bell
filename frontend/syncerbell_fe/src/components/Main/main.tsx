841fcd66714688a6eb419611abb748c706795d6b
JH9892 <jhchoi09@naver.com>
2021-12-30 17:59:33 +0900

    save

---
+++ frontend/syncerbell_fe/src/components/Main/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dashboard from '../Dashboard/Dashboard'
+import Sidebar from '../Sidebar/Sidebar'
+
+const main = () => {
+    return (
+        <div>
+            <Sidebar />
+            <Dashboard />
+        </div>
+    )
+}
+
+export default main
