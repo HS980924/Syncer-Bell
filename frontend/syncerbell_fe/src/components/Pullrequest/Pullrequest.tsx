8e52e10f83b488b7520fc432a28ee8423c57ea58
JH9892 <jhchoi09@naver.com>
2022-01-20 20:00:19 +0900

    0120 Work : Pull Request function Successgit add *

--- frontend/syncerbell_fe/src/components/Pullrequest/Pullrequest.tsx
+++ frontend/syncerbell_fe/src/components/Pullrequest/Pullrequest.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import {useState, useEffect} from 'react';
 import styles from './Pullrequest.module.scss';
 import RightSide from '../RightSide/RightSide';
-import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowLeft, FaSyncAlt, FaBoxes } from 'react-icons/fa';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+let prData=[{repoName:"", user:"", message:"", date:"", url:""}];
+export let printPr=[{repoName:"", user:"", message:"", date:"", url:""}];
 
 const Pullrequest = () => {
+    const [pr, setPr] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [errors, setError] = useState<any | null>(null);
+
+    const fetchPullRequest = async()=>{
+        try{
+            setError(null);
+            setPr(null);
+            setLoading(true);
+            const res = await axios.get('/pullrequest');
+            setPr(res.data);
+            console.log(res.data);
+        } catch(error){
+            setError(error);
+        }
+        setLoading(false);
+    }
+
+    useEffect(()=>{
+        fetchPullRequest();
+    }, []);
+
+    if(loading) return <h3 className={styles.msg}>Loading....</h3>;
+    if(errors) return <h3 className={styles.msg}>Error!!</h3>;
+    if(!pr) return null;
+
+    prData = JSON.parse(pr);
+    printPr = prData.slice(0,10)
+
     return (
         <>
             <main className={styles.dashBoard}>
@@ -18,18 +52,38 @@ const Pullrequest = () => {
                             ---------------------------->*/}
                         <div className={styles.headerArea}>
                             <div className={styles.headTitleArea}>
-                                <p className={styles.title}>Syncer-Bell</p>
-                                <p className={styles.dataRange}>01 - 31, January, 2022</p>
+                                <div className={styles.titleGroup}>
+                                    <FaBoxes className={styles.prIcon}/>
+                                    <p className={styles.title}>Pull Request</p>
+                                    <p className={styles.dates}>January 1th ~ 30th, 2022</p>
+                                </div>
+                                <FaSyncAlt className={styles.prReloadIcon}/>
                             </div>
                         </div>
 
                         {/*<!-------------------------- 
-                                Pull Request Area 
+                                    Pull Request Area 
                             ---------------------------->*/}
-                        <div className={styles.issueArea}>
-                            <div className={styles.titleArea}>
-                                <p className={styles.areaTitle}>Pull Request</p>
-                            </div>
+                        <div className={styles.prArea}>
+                            <ul className={styles.listArea}>
+                                {printPr.map((item)=>(
+                                    <li className={styles.prList} key={item.message} onClick={()=>{
+                                        window.open(`${item.url}`, '_blank')
+                                    }}>
+                                        <div className={styles.issueListItem}>
+                                                <div className={styles.itemHeader}>
+                                                    <p className={styles.prTitle}>{item.message}</p>
+                                                    <p className={styles.prTime}>{item.date}</p>
+                                                    <div className={styles.secondInfo}>
+                                                        <p className={styles.prRepo}>{item.repoName}</p>
+                                                        <p className={styles.prAsignee}>{item.user}</p>
+                                                    </div>
+                                                </div>
+                                        </div>
+                                        <p className={styles.prBio}>{item.message}</p>
+                                    </li>
+                                ))}
+                            </ul>
                         </div>
                     </section>
                     <RightSide/>
