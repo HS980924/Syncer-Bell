dee114d17160ebb5b62fc744304dbfc025b273ff
choihs <choihs1998@naver.com>
2022-01-23 21:02:55 +0900

    Add routing path and function file

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const githubServiceCommit = require('../services/getuser.js');
+const githubServiceCommit = require('../services/getcommit.js');
 const githubServiceIssue = require('../services/getissue.js');
 const githubServicePull = require('../services/getpulls.js');
+const githubServiceUser = require('../services/getuser.js');
 
 router.get('/home', async (req,res) => {
-    const leftSide = githubServiceCommit.UserInfo(req.session.passport.user.profile._json)
+    const leftSide = githubServiceUser.UserInfo(req.session.passport.user.profile._json)
     
     res.json(leftSide);
 });
@@ -14,25 +15,38 @@ router.get('/commit', async(req,res) => {
     const Token = req.session.passport.user.token;
     const userId = req.session.passport.user.profile.username;
     const myCommit = await githubServiceCommit.getUserCommit(userId,Token)
-    
-    res.json(myCommit)
+    const result = githubServiceUser.getRefineData(myCommit)
+
+    res.json(JSON.stringify(result))
 })
 
 router.get('/issue',async(req,res) => {
     const Token = req.session.passport.user.token;
     const userId = req.session.passport.user.profile.username;
     const myIssue = await githubServiceIssue.getUserIssue(userId,Token) 
+    const result = githubServiceUser.getRefineData(myIssue)
 
-
-    res.json(myIssue)
+    res.json(JSON.stringify(result))
 })
 
-router.get('/pullrequest',async(req,res) => {
+router.get('/pullrequest', async(req,res) => {
     const Token = req.session.passport.user.token;
     const userId = req.session.passport.user.profile.username;
-    const mypulls = await githubServicePull.getUserPull(userId,Token) 
+    const mypulls = await githubServicePull.getUserPull(userId,Token)
+    const result = githubServiceUser.getRefineData(mypulls)
 
-    res.json(mypulls)
+    res.json(JSON.stringify(result))
+})
+
+router.get('/alldata', async(req,res) => {
+    const Token = req.session.passport.user.token;
+    const userId = req.session.passport.user.profile.username;
+    const myCommit = await githubServiceCommit.getUserCommit(userId,Token)
+    const myIssue = await githubServiceIssue.getUserIssue(userId,Token)
+    const mypulls = await githubServicePull.getUserPull(userId,Token)
+    const result = githubServiceUser.AllData(myCommit, myIssue, mypulls)
+
+    res.json(JSON.stringify(result))
 })
 
 module.exports = router;
